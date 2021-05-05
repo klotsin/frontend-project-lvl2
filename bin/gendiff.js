e50072ecd0216cb87810601a6b0268d768e2e3af
Ermakov <klotsin2010@mail.ru>
2021-05-05 23:56:29 +0400

    add help --format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,4 +8,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
   .version('0.0.1')
+  .option('-f, --format [type]', 'output format (default: "stylish")')
+  .arguments('<filepath1>')
+  .arguments('<filepath2>')
   .parse(process.argv);
